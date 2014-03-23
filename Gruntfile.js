c20a668403a48576d63382b6e196ebf87d577ff7
ruanyl <ruanyu1@gmail.com>
2014-03-23 12:33:58 +0200

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js']
+    },
+    watch: {
+      files: ['src/*.js'],
+      tasks: ['jshint', 'browserify']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '.'
+        }
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'static/js/client.js': ['src/*.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['browserify', 'connect', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+Nodejs Project Template
+=============
+
+Base nodejs project template
+
+Clone the repo and use npm to install the other dependencies:
+
+```
+git clone <repo url>
+cd <project dir>
+npm install
+npm install -g grunt-cli
+```
+
+Run:
+```
+grunt
+```
+
+View server at localhost:8080
