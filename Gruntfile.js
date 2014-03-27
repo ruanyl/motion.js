fb4994d631ca688f8dfc4b015720c4eae6869c22
ruanyl <ruanyu1@gmail.com>
2014-03-27 21:33:29 +0200

    add mocha test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/*.js']
+      files: ['Gruntfile.js', 'src/*.js'],
+      test: ['Gruntfile.js', 'test/**/*.js']
     },
     watch: {
       files: ['src/*.js'],
@@ -22,12 +23,22 @@ module.exports = function(grunt) {
           'static/js/client.js': ['src/*.js']
         }
       }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('default', ['browserify', 'jshint', 'connect', 'watch']);
+  grunt.registerTask('test', ['browserify', 'jshint', 'mochaTest']);
 };
