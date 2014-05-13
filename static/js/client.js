f8f4a8ee12c05bcb711ea72bb15d62d6123aeed7
ruanyl <ruanyu1@gmail.com>
2014-05-13 23:19:59 +0300

    add demo

---
+++ static/js/client.js
@@ -0,0 +1,4 @@
+var stage = new Motion.Stage('canvas');
+var rect1 = new Motion.Rect({x:30, y:30, width:30, height:30});
+var rect2 = new Motion.Rect({x:60, y:60, width:30, height:30});
+stage.add(rect1, rect2);
